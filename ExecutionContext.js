f03e51330b64f76f455f1088140091b9f82f7358
Sairam-2189 <88224231+Sairam-2189@users.noreply.github.com>
2024-06-20 20:09:23 +0530

    Create ExecutionContext.js

---
+++ ExecutionContext.js
@@ -0,0 +1,11 @@
+// * Everything in JavaScript happens in an Execution Context.
+
+// * Execution context is a big box or container where javascript code is executed.
+// * It has 2 components.
+// * 1. Memory component - This is a place where variables and functions are
+// * stored in key-value pairs. This is also called as variable environment.
+// * 2. Code component   - This is the place where code is executed one line
+// * at a time. This is also called as Thread of execution.
+
+// * Javascript is known as Synchronous Single Threaded Language.
+// * Javascript can only execute one command at a time in a specific order(line by line).
